84036b483d49a12a240a0643e1c5f7b4a7dd773a
Arizfurinmoto <sz.godon@gmail.com>
2022-12-30 15:56:10 +0100

    products data moved to separated file

---
+++ ski-shop/src/products.js
@@ -0,0 +1,22 @@
+const products = [
+  {
+		id: 1,
+		name: "Narty Salomon",
+		rating: 3.7,
+		description:
+			"Super narty na zimę i na lato.",
+		price: 500,
+		image: require("./assets/images/product-1.jpg"),
+	},
+  {
+		id: 2,
+		name: "Kask Rossignol Jednorożec",
+		rating: 5.0,
+		description:
+			"Znakomity kask dla dziewczynek w wieku do 10 lat i studentów informatyki w wieku 20-26 lat",
+		price: 420.20,
+		image: require("./assets/images/product-2.png"),
+	},
+]
+
+export default products
