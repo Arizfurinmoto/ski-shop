e78d19efa1e410688c0f9454c2f4f5872afdbf08
Arizfurinmoto <sz.godon@gmail.com>
2022-12-30 17:01:49 +0100

    styles changed

--- ski-shop/src/App.js
+++ ski-shop/src/App.js
@@ -75,29 +75,30 @@ function App() {
           Products
         </h2>
         <div className="products">
+
           {products.map((product) => (
-            <div
-              className="product"
-              key={product.id}>
-              <img
-              className="prodyct-image"
+            <div className="product" key={product.id}>
+
+              <img className="product-image"
               src={product.image}
-              alt={product.image}
-              />
+              alt={product.image}/>
+
               <h4 className="product-name">{product.name}</h4>
           
-          <RatingStars rating={product.rating} />
-          <p>{product.description}</p>
-          <span className="product-price">{product.price}$</span>
-          <div className="buttons">
-            <button className="btn">Detail</button>
-            <button className="btn" 
-              onClick={() => addProductToCart(product)}>
-              Add to cart
-              </button>
-          </div>
-        </div>
+              <RatingStars rating={product.rating} />
+
+              <p>{product.description}</p>
+
+              <span className="product-price">{product.price}$</span>
+          
+              <div className="buttons">
+                <button className="btn">Detail</button>
+                <button className="btn" onClick={() => addProductToCart(product)}>Add to cart</button>
+              </div>
+
+            </div>
         ))}
+
         </div>
       </main>
     </div>
--- ski-shop/src/products.js
+++ ski-shop/src/products.js
@@ -1,22 +1,126 @@
-const products = [
-  {
+
+const products = [{
 		id: 1,
 		name: "Narty Salomon",
 		rating: 3.7,
-		description:
-			"Super narty na zimę i na lato.",
+		description: "Super narty na zimę i na lato.",
 		price: 500,
 		image: require("./assets/images/product-1.jpg"),
 	},
-  {
+	{
 		id: 2,
 		name: "Kask Rossignol Jednorożec",
 		rating: 5.0,
-		description:
-			"Znakomity kask dla dziewczynek w wieku do 10 lat i studentów informatyki w wieku 20-26 lat",
+		description: "Znakomity kask dla dziewczynek w wieku do 10 lat i studentów informatyki w wieku 20-26 lat",
 		price: 420.20,
-		image: require("./assets/images/product-2.png"),
+		image: require("./assets/images/product-2.jpg"),
 	},
+	{
+		id: 3,
+		name: "Kask Rossignol Jednorożec",
+		rating: 5.0,
+		description: "Znakomity kask dla dziewczynek w wieku do 10 lat i studentów informatyki w wieku 20-26 lat",
+		price: 420.20,
+		image: require("./assets/images/product-3.jpg"),
+	},
+	{
+		id: 4,
+		name: "Kask Rossignol Jednorożec",
+		rating: 5.0,
+		description: "Znakomity kask dla dziewczynek w wieku do 10 lat i studentów informatyki w wieku 20-26 lat",
+		price: 420.20,
+		image: require("./assets/images/product-4.jpg"),
+	},
+	{
+		id: 5,
+		name: "Kask Rossignol Jednorożec",
+		rating: 5.0,
+		description: "Znakomity kask dla dziewczynek w wieku do 10 lat i studentów informatyki w wieku 20-26 lat",
+		price: 420.20,
+		image: require("./assets/images/product-5.jpg"),
+	},
+	{
+		id: 6,
+		name: "Kask Rossignol Jednorożec",
+		rating: 5.0,
+		description: "Znakomity kask dla dziewczynek w wieku do 10 lat i studentów informatyki w wieku 20-26 lat",
+		price: 420.20,
+		image: require("./assets/images/product-6.jpg"),
+	},
+	{
+		id: 7,
+		name: "Kask Rossignol Jednorożec",
+		rating: 5.0,
+		description: "Znakomity kask dla dziewczynek w wieku do 10 lat i studentów informatyki w wieku 20-26 lat",
+		price: 420.20,
+		image: require("./assets/images/product-7.jpg"),
+	},
+	{
+		id: 8,
+		name: "Kask Rossignol Jednorożec",
+		rating: 5.0,
+		description: "Znakomity kask dla dziewczynek w wieku do 10 lat i studentów informatyki w wieku 20-26 lat",
+		price: 420.20,
+		image: require("./assets/images/product-8.jpg"),
+	},
+	{
+		id: 9,
+		name: "Kask Rossignol Jednorożec",
+		rating: 5.0,
+		description: "Znakomity kask dla dziewczynek w wieku do 10 lat i studentów informatyki w wieku 20-26 lat",
+		price: 420.20,
+		image: require("./assets/images/product-9.jpg"),
+	},
+	{
+		id: 10,
+		name: "Kask Rossignol Jednorożec",
+		rating: 5.0,
+		description: "Znakomity kask dla dziewczynek w wieku do 10 lat i studentów informatyki w wieku 20-26 lat",
+		price: 420.20,
+		image: require("./assets/images/product-10.jpg"),
+	},
+	{
+		id: 11,
+		name: "Kask Rossignol Jednorożec",
+		rating: 5.0,
+		description: "Znakomity kask dla dziewczynek w wieku do 10 lat i studentów informatyki w wieku 20-26 lat",
+		price: 420.20,
+		image: require("./assets/images/product-11.jpg"),
+	},
+	{
+		id: 12,
+		name: "Kask Rossignol Jednorożec",
+		rating: 5.0,
+		description: "Znakomity kask dla dziewczynek w wieku do 10 lat i studentów informatyki w wieku 20-26 lat",
+		price: 420.20,
+		image: require("./assets/images/product-12.jpg"),
+	},
+	{
+		id: 13,
+		name: "Kask Rossignol Jednorożec",
+		rating: 5.0,
+		description: "Znakomity kask dla dziewczynek w wieku do 10 lat i studentów informatyki w wieku 20-26 lat",
+		price: 420.20,
+		image: require("./assets/images/product-13.jpg"),
+	},
+	{
+		id: 14,
+		name: "Kask Rossignol Jednorożec",
+		rating: 5.0,
+		description: "Znakomity kask dla dziewczynek w wieku do 10 lat i studentów informatyki w wieku 20-26 lat",
+		price: 420.20,
+		image: require("./assets/images/product-14.jpg"),
+	},
+	{
+		id: 15,
+		name: "Kask Rossignol Jednorożec",
+		rating: 5.0,
+		description: "Znakomity kask dla dziewczynek w wieku do 10 lat i studentów informatyki w wieku 20-26 lat",
+		price: 420.20,
+		image: require("./assets/images/product-15.jpg"),
+	},
+
+
 ]
 
 export default products
