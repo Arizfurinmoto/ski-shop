c8cf8f38904600bcd3b952dfc9513581cf656eb1
Arizfurinmoto <sz.godon@gmail.com>
2022-12-30 13:14:13 +0100

    ShoppingCart component added

--- ski-shop/src/App.js
+++ ski-shop/src/App.js
@@ -5,12 +5,21 @@ import './App.css';
 const products = [
   {
 		id: 1,
-		name: "Vivamus vitae",
-		rating: 4.3,
+		name: "Narty Salomon",
+		rating: 3.7,
 		description:
-			"Vivamus vitae neque accumsan, ultrices nisl et, viverra magna. Fusce nec maximus sem.",
-		price: 199,
-		image: require("./assets/images/product-1.png"),
+			"Super narty na zimę i na lato.",
+		price: 500,
+		image: require("./assets/images/product-1.jpg"),
+	},
+  {
+		id: 2,
+		name: "Kask Rossignol Jednorożec",
+		rating: 5.0,
+		description:
+			"Znakomity kask dla dziewczynek w wieku do 10 lat i studentów informatyki w wieku 20-26 lat",
+		price: 420.20,
+		image: require("./assets/images/product-2.png"),
 	},
 ]
 
