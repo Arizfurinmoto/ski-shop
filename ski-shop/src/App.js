a15d0bca2eb711515deeed0380dbee9997eb61a6
Arizfurinmoto <sz.godon@gmail.com>
2022-12-30 15:53:13 +0100

    products data moved to separate file

--- ski-shop/src/App.js
+++ ski-shop/src/App.js
@@ -3,28 +3,8 @@ import RatingStars from "./components/RatingStars.js"
 import './App.css';
 import { useState, useEffect } from "react";
 import ShoppingCart from "./components/ShoppingCart.js";
+import products from "./products.js"
 
-const products = [
-  {
-		id: 1,
-		name: "Narty Salomon",
-		rating: 3.7,
-		description:
-			"Super narty na zimę i na lato.",
-		price: 500,
-		image: require("./assets/images/product-1.jpg"),
-	},
-  {
-		id: 2,
-		name: "Kask Rossignol Jednorożec",
-		rating: 5.0,
-		description:
-			"Znakomity kask dla dziewczynek w wieku do 10 lat i studentów informatyki w wieku 20-26 lat",
-		price: 420.20,
-		image: require("./assets/images/product-2.png"),
-	},
-]
-//void start(){Console.WriteLine("")}
 function App() {
 
   const[cartsVisibility, setCartVisible] = useState(false);
