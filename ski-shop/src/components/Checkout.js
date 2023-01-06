7f493c279ec2183b49399612e17860ba16e3d5b4
Arizfurinmoto <sz.godon@gmail.com>
2023-01-06 13:02:20 +0100

    checkout component style updated

--- ski-shop/src/components/Checkout.js
+++ ski-shop/src/components/Checkout.js
@@ -1,9 +1,22 @@
+import {AiFillCloseCircle} from "react-icons/ai"
+import "../checkout.css"
 
 const Checkout = ({visibility, onClose}) => {
     return(
         <div style={{display: visibility ? "block" : "none"}}>
-            <h1>Hi I am going to be the Checkout modal!</h1>
-            <button onClick={onClose}>Close</button>
+            <div className="checoutModal" style={{display: visibility ? "block" : "none"}}>
+            <div className="checkout">
+
+                <header className="header">
+                    <div className="titleContainer">
+                        <h2>Checkout</h2>
+                    </div>
+                    <button className="close-btn" onClick={onClose}>
+                        <AiFillCloseCircle size={30}/>
+                    </button>
+                </header>
+            </div>
+        </div>
         </div>
     )
 }
